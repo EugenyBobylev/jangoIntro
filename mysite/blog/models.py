95b8ed3f7b242409941efe7370333598b82c34a0
Eugeny Bobylev <ebobylev@gmail.com>
2020-09-23 19:19:59 +0300

    fixed errors in Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -9,7 +9,7 @@ class Post(models.Model):
         ('published', 'Published')
     )
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='Publish')
+    slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
